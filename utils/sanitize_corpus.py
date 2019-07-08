d33932865cedfaf221ee929617b2dfe458685642
Steven <ssleder@iastate.edu>
2019-07-08 10:34:37 -0500

    Wrote the dataset concatinator.

--- utils/sanitize_corpus.py
+++ utils/sanitize_corpus.py
@@ -1,48 +1,60 @@
 # Utilize this entry point to sanitize a file of all punctuation, stop words 
 #(by a flag), set characters to lowercase, and remove numbers.
 
-import re
 
-# Description: Non-helper method to call for sanitizing an input text corpus.
+import csv
+import re
+import sys
+
+import pandas as pd
+
+csv.field_size_limit(sys.maxsize)
+
+# Description: Non-helper method to call for sanitizing an input text entry.
 #
 # Arguments: 
-#   corpus: Input array of sentences
+#   entry: Input array of sentences
 #   save_stopwords: flag for whether or not to leave stopwords
 #
-# Return: Corpus: Array of sanitized sentences
-def sanitize_corpus(corpus, save_stopwords = False):
+# Return: data: Array of sanitized sentences
+def sanitized_dataset(data, min_frequency = 50, save_stopwords = False):
 
-    # remove all stopwords if the flag is set
-    if (not remove_stopwords):
-        corpus = remove_stopwords(corpus)
+    freq = []
 
-    # remove all punctuation (TODO: decide whether to remove periods)
-    corpus = remove_punctuation(corpus)
+    for e, row in enumerate(data):
+        # remove all punctuation (TODO: decide whether to remove periods)
+        #entry = remove_punctuation(entry)
+    
+        # remove all numerals
+        data[e] = remove_numbers(date[e])
+        
+        # strip all extraneous whitespace
+        data[e] = reduce_whitespace(data[e])
+    
+        # all to lowercase
+        data[e] = data[e].lower()
+        
+        # remove all stopwords if the flag is set
+        if (not remove_stopwords):
+            data[e] = remove_stopwords(data[e])
+    
+        print(data[e])
+        print('\n')
 
-    # remove all numerals
-    corpus = remove_numbers(corpus)
-
-    # strip all extraneous whitespace
-    corpus = reduce_whitespace(corpus)
-
-    # all to lowercase
-    corpus = corpus.lower()
-
-    return corpus
+    return data
 
 # TODO: Implement
-def remove_stopwords(corpus):
-    return corpus
+def remove_stopwords(entry):
+    return entry
 
 # TODO: Implement, decide whether to save periods
-def remove_punctuation(corpus):
-    return corpus
+def remove_punctuation(entry):
+    return entry
 
-def remove_numbers(corpus):
-    corpus = re.sub(r'\d+', '', corpus)
-
-    return corpus
+def remove_numbers(entry):
+    return re.sub(r'\d+', '', entry)
 
 # TODO: Implement
-def reduce_whitespace(corpus):
-    return corpus
+def reduce_whitespace(entry):
+    return re.sub(" +", " ", entry)
+
